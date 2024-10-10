8c5169e5b4e735631fe48db58c88e578a4fef41b
gor3000 <aleveri3000@gmail.com>
2024-10-10 10:21:11 +0200

    message  2222222222222

---
+++ ghhhg/src/gurj/memezida.java
@@ -0,0 +1,13 @@
+package gurj;
+
+public class memezida {
+
+	public static void main(String[] args) {
+		System.out.println("paragvai");
+		System.out.println("paragvai");
+		System.out.println("paragvai");
+		System.out.println("paragvai");
+
+	}
+
+}
